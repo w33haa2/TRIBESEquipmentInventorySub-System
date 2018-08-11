5906d992a820ba34ea16bed8e0386a4a7bbfc390
Eugenio Julius Malasaga <33626036+supernig@users.noreply.github.com>
2018-08-11 11:17:41 +0800

    test

--- WindowsFormsApplication2/UserControl1.Designer.cs
+++ WindowsFormsApplication2/UserControl1.Designer.cs
@@ -46,7 +46,7 @@
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(88, 21);
             this.button1.TabIndex = 0;
-            this.button1.Text = "CLEAR ALL";
+            this.button1.Text = "ALL";
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
